e0efd03d6eb93b197cce75a610c6040a2d1c53c6
Joey Feng <jfeng702@gmail.com>
2018-01-31 09:36:29 -0800

    add greeting component

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionApiUtil from './util/session_api_util';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Slice</h1>, root);
   const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+
+  // TESTING START
+window.getState = store.getState;
+window.dispatch = store.dispatch;
+// TESTING END
+
+  ReactDOM.render(<Root store={store}/>, root);
+
 });
