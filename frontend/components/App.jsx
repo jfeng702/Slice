d8719bfd550929bf1b070afc0ce824b4bdaa3a2d
Joey Feng <jfeng702@gmail.com>
2018-02-01 13:59:48 -0800

    initial modal

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -9,15 +9,17 @@ import {
 } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
+import SplashContainer from './splash/splash_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
     <header>
+      {/* 
       <Link to="/" className="header-link">
         <h1>Slice</h1>
       </Link>
-      <GreetingContainer/>
+      <GreetingContainer/> */}
     </header>
     <Switch>
       <AuthRoute exact path="/login" component={SessionFormContainer} />
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ReactModal from 'react-modal';
 //has a nav bar, Sign Up button, and text in the center.
 //no need for state right? just links
 
-const Splash = () => (
-  <div className="splash">
-    <Link to="/login">Login</Link>
-    <Link to="/signup">Sign Up</Link>
-  </div>
-);
+class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal () {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal () {
+    this.setState({ showModal: false });
+  }
+
+  render () {
+    return (
+      <div className="splash">
+        <div className="splash-nav">
+          <Link to="/login">Log In</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+
 
 export default Splash;
