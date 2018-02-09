a03c075463ceeba9a6b729502e2513624a1f08ec
Joey Feng <jfeng702@gmail.com>
2018-02-09 11:32:48 -0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,243 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Photo.destroy_all
-# User.destroy_all
-#
-# user1 = User.create!({ username: 'test1', password: 'password' })
-#
-# user2 = User.create!({ username: 'test2', password: 'password'})
-#
+Photo.destroy_all
+User.destroy_all
+AlbumPhoto.destroy_all
+Album.destroy_all
+
+# Users
+user1 = User.create!({ username: 'guest', password: 'password' })
+user2 = User.create!({ username: 'JamesBond', password: 'password'})
+
+# Photos
+# Beautiful Sights
+ photo1 = Photo.create!({img_url: 'http://res.cloudinary.com/slicecloud/image/upload/v1518196108/pexels-photo-326055_tclf23.jpg', title: 'Blue butterflies', owner_id: user1.id })
+ photo2 = Photo.create!({img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518196107/italy-2080072_960_720_j41gyb.jpg', title: 'Snow', owner_id: user1.id})
+ photo3 = Photo.create!({img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518196107/pexels-photo-206359_dxcnlg.jpg', owner_id:user1.id})
+ photo4 = Photo.create!({img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518196107/11671_djakcp.jpg', owner_id:user1.id})
+ photo5 = Photo.create!({img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518196107/1202759-beautiful-wallpaper_yvtlca.jpg', owner_id:user1.id})
+ photo6 = Photo.create!({img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518196107/Beautiful-Wallpapers-55G_wvg5ak.jpg', owner_id:user2.id})
+ photo7 = Photo.create!({img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518196106/224340939-pictures-beautiful_jbilvd.jpg', owner_id:user2.id})
+ photo8 = Photo.create!({img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518196107/1563803_asq90z.jpg', owner_id: user2.id})
+
+# Animals
+
+  photo9 = Photo.create!(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518201220/Polar_Bear_Threats_Image3_202798_h4jgyg.jpg',
+    title: 'Polar Bears',
+    owner_id: user1.id
+  )
+  photo10 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518201220/01-animal-speed-cheetah.ngsversion.1500303651696.adapt.1900.1_z3v0zc.jpg',
+    title: 'Cheetah',
+    owner_id: user1.id
+  )
+  photo11 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518201220/01-booktalk-animal-personalities-NationalGeographic_2495705_e2kx8q.jpg',
+    title: '',
+    owner_id: user1.id
+  )
+  photo12 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518201220/category-thumbnail-mammals_0_lxwezw.jpg',
+    title: 'Panda',
+    owner_id: user1.id
+  )
+  photo13 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518201219/cecil-AP463227356214-1000x400_q8oz2p.jpg',
+    title: 'Lion',
+    owner_id: user1.id
+  )
+  photo14 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518201224/nature-bird-flying-red_zkdiec.jpg',
+    title: 'Parrot',
+    owner_id: user1.id
+  )
+  photo15 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518201229/pexels-photo-145939_c4dnlp.jpg',
+    title: 'Tiger',
+    owner_id: user1.id
+  )
 
 
-# photos = Photo.create!([{ img_url: 'https://static.pexels.com/photos/46710/pexels-photo-46710.jpeg', owner_id: user1.id, title: 'awesome pic', description: 'taken with nikon' },
-#   {img_url: 'https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg', owner_id: user1.id, title: 'awesome pic', description: 'taken with nikon'},
-#   {img_url: 'https://static.pexels.com/photos/34950/pexels-photo.jpg', owner_id: user1.id, title: 'awesome pic', description: 'taken with nikon'},
-#   {img_url: 'http://i.dailymail.co.uk/i/pix/2016/09/06/11/37F60FD200000578-0-image-a-5_1473156426673.jpg', owner_id: user1.id, title: 'awesome pic', description: 'taken with nikon'},
-#   {img_url: 'https://as.ftcdn.net/r/v1/pics/ea2e0032c156b2d3b52fa9a05fe30dedcb0c47e3/landing/images_photos.jpg', owner_id: user2.id, title: 'awesome pic', description: 'taken with nikon'},
-#   {img_url: 'http://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg', owner_id: user2.id, title: 'awesome pic', description: 'taken with nikon'},
-#   {img_url: 'https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg', owner_id: user2.id, title: 'awesome pic', description: 'taken with nikon'},
-#   {img_url: 'https://www.wonderplugin.com/videos/demo-image0.jpg', owner_id: user2.id, title: 'awesome pic', description: 'taken with nikon'},
-#   {img_url: 'https://images.freeimages.com/images/small-previews/25d/eagle-1523807.jpg', owner_id: user2.id, title: 'awesome pic', description: 'taken with nikon'}
-#  ])
 
- Photo.create!([{img_url: 'http://www.hd-freewallpapers.com/free-parrots-wallpapers/beautiful-images-of-parrots-wallpaper.jpg', owner_id: 16 },
-   {img_url:'https://www.thephotoargus.com/wp-content/uploads/2016/12/peacock-9.jpg', owner_id:16},
-   {img_url:'http://cdn2.stylecraze.com/wp-content/uploads/2013/08/1791_25-Most-Beautiful-Pink-Roses_-is-tock-675065828.jpg', owner_id:16},
-   {img_url:'http://hdfreewallpaper.net/wp-content/uploads/2015/10/beautiful-waterfall-and-pink-flowers-free-hd-for-desktop.jpg', owner_id:16},
-   {img_url:'https://static.pexels.com/photos/326055/pexels-photo-326055.jpeg', owner_id:16},
-   {img_url:'https://cdn.pixabay.com/photo/2017/02/19/15/28/italy-2080072_960_720.jpg', owner_id:16},
-   {img_url:'https://static.pexels.com/photos/206359/pexels-photo-206359.jpeg', owner_id:16}])
+  # Landscape
+  photo16 = Photo.create(
+    img_url: 'http://res.cloudinary.com/slicecloud/image/upload/v1518202551/pexels-photo_ufdx5s.jpg',
+    title: 'Along the emerald road',
+    owner_id: user1.id
+  )
+  photo17 = Photo.create(
+    img_url: 'http://res.cloudinary.com/slicecloud/image/upload/v1518202548/award-winning-stradivarius-violin-pool-cipriano-landscape-design-2-blue-lights_qf8tfe.jpg',
+    title: 'Exotic Pool',
+    owner_id: user1.id
+  )
+  photo18 = Photo.create(
+    img_url: 'http://res.cloudinary.com/slicecloud/image/upload/v1518202548/pexels-photo-248797_wvk3jl.jpg',
+    title: 'Where I wish I was right now',
+    owner_id: user1.id
+  )
+  photo19 = Photo.create(
+    img_url: 'http://res.cloudinary.com/slicecloud/image/upload/v1518202548/amazing-animal-beautiful-beautifull_b400yj.jpg',
+    title: '',
+    owner_id: user1.id
+  )
+  photo20 = Photo.create(
+    img_url: 'http://res.cloudinary.com/slicecloud/image/upload/v1518202548/37F60FD200000578-0-image-a-5_1473156426673_gic86b.jpg',
+    title: '',
+    owner_id: user1.id
+  )
 
-# album_photos = AlbumPhoto.create!([{ album_id: album_id, photo_id: 75 },
+  # New Animals
+  photo21 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518200793/orangutan_1600x1000_279157_tr2tf3.jpg',
+    title: 'Orangutan',
+    owner_id: user1.id
+  )
+  photo22 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518200792/enhanced-buzz-1492-1379411828-15_r2ksk6.jpg',
+    title: 'Little Hedgehog',
+    owner_id: user1.id
+  )
+  photo23 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518200792/demo-image0_hebv2f.jpg',
+    title:  'Swan',
+    owner_id: user1.id
+  )
+  photo24 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518200792/eagle-1523807_mtevwt.jpg',
+    title: 'Eagle',
+    description: 'This is the look of freedom',
+    owner_id: user1.id
+  )
+  photo25 = Photo.create(
+    img_url:'http://res.cloudinary.com/slicecloud/image/upload/v1518196106/beautiful-images-of-parrots-wallpaper_b8davu.jpg',
+    title: 'Birds',
+    owner_id: user1.id
+  )
+
+album1 = Album.create!({title: 'Beautiful Sights', owner_id: user1.id})
+album2 = Album.create!(title: 'Animals', owner_id: user1.id)
+album3 = Album.create!(title: 'Round The World', owner_id: user1.id)
+
+AlbumPhoto.create!(
+  album_id: album1.id,
+  photo_id: photo1.id
+)
+
+AlbumPhoto.create!(
+  album_id: album1.id,
+  photo_id: photo2.id
+)
+
+AlbumPhoto.create!(
+  album_id: album1.id,
+  photo_id: photo3.id
+)
+
+AlbumPhoto.create!(
+  album_id: album1.id,
+  photo_id: photo4.id
+)
+
+AlbumPhoto.create!(
+  album_id: album1.id,
+  photo_id: photo5.id
+)
+
+AlbumPhoto.create!(
+  album_id: album1.id,
+  photo_id: photo6.id
+)
+
+AlbumPhoto.create!(
+  album_id: album1.id,
+  photo_id: photo7.id
+)
+
+AlbumPhoto.create!(
+  album_id: album1.id,
+  photo_id: photo8.id
+)
+
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo9.id
+)
+
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo10.id
+)
+
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo11.id
+)
+
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo12.id
+)
+
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo13.id
+)
+
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo14.id
+)
+
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo15.id
+)
+AlbumPhoto.create!(
+  album_id: album3.id,
+  photo_id: photo16.id
+)
+AlbumPhoto.create!(
+  album_id: album3.id,
+  photo_id: photo17.id
+)
+AlbumPhoto.create!(
+  album_id: album3.id,
+  photo_id: photo18.id
+)
+AlbumPhoto.create!(
+  album_id: album3.id,
+  photo_id: photo19.id
+)
+AlbumPhoto.create!(
+  album_id: album3.id,
+  photo_id: photo20.id
+)
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo21.id
+)
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo22.id
+)
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo23.id
+)
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo24.id
+)
+AlbumPhoto.create!(
+  album_id: album2.id,
+  photo_id: photo25.id
+)
 
 
-beautiful_pics = Album.create!([{title: 'Beautiful Sights', owner_id: 16}])
+
+
+
+
+# album_photos = AlbumPhoto.create([{ album_id: album_id, photo_id: 75 },
