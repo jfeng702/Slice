fd6a7dc23715b2febf644f126516995aab83d283
Joey Feng <jfeng702@gmail.com>
2018-03-20 22:28:04 -0700

    masonry turned off fitwidth for image render fix

--- frontend/components/photo/photo_index.jsx
+++ frontend/components/photo/photo_index.jsx
@@ -5,24 +5,27 @@ import { Link } from 'react-router-dom';
 
 var masonryOptions = {
     // transitionDuration: '0.8s',
-    fitWidth: true,
+    fitWidth: false,
     // horizontalOrder: true,
     // stagger: 90,
-    itemSelector: '.container',
+    itemSelector: '.photo-index-container',
     gutter:0,
     // columnWidth: '.grid-sizer'
 };
 
 class PhotoIndex extends React.Component {
+
   componentDidMount() {
     this.props.fetchPhotos();
   }
+
+
 
   render() {
     var childElements = this.props.photos.map(function(photo){
        return (
             <Link className="photo-index-link" key={photo.id} to={`/photos/${photo.id}`}>
-              <div className="container">
+              <div className="photo-index-container">
                 <img className="photo-index-child" src={photo.img_url}/>
                 <div className="caption-overlay">
                   <h1>{photo.title} by {photo.ownerName}</h1>
@@ -31,19 +34,47 @@ class PhotoIndex extends React.Component {
             </Link>
         );
     });
+  return (
+    <Masonry
+      className={'my-gallery-class'}
+      elementType={'div'}
+      options={masonryOptions}
+      disableImagesLoaded={false}
+      updateOnEachImageLoad={false}
+    >
+      <div className="grid-sizer"></div>
+        {childElements}
+    </Masonry>
+  );
+    //
+    // render() {
+    //   var elem = document.querySelector('.my-gallery-class');
+    //   var msnry = new Masonry( elem, {
+    //     // options
+    //     itemSelector: '.photo-index-container',
+    //     // columnWidth: 200
+    //   });
+    //
+    //   var childElements = this.props.photos.map(function(photo){
+    //      return (
+    //           <Link className="photo-index-link" key={photo.id} to={`/photos/${photo.id}`}>
+    //             <div className="photo-index-container">
+    //               <img className="photo-index-child" src={photo.img_url}/>
+    //               <div className="caption-overlay">
+    //                 <h1>{photo.title} by {photo.ownerName}</h1>
+    //               </div>
+    //             </div>
+    //           </Link>
+    //       );
+    //   });
+    // return(
+    //   <div className="my-gallery-class">
+    //     <div className="grid-sizer"></div>
+    //       {childElements}
+    //   </div>
+    // );
 
-    return (
-      <Masonry
-        className={'my-gallery-class'}
-        elementType={'div'}
-        options={masonryOptions}
-        disableImagesLoaded={false}
-        updateOnEachImageLoad={false}
-      >
-        <div className="grid-sizer"></div>
-          {childElements}
-      </Masonry>
-    );
+
   }
 }
 
