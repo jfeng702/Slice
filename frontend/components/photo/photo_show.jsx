c03672bc598708163187d85411575be10620e472
Joey Feng <jfeng702@gmail.com>
2018-05-18 09:01:11 -0700

    photo show style, converting to flex

--- frontend/components/photo/photo_show.jsx
+++ frontend/components/photo/photo_show.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import CommentIndexContainer from '../comment/comment_index_container';
 import TagIndexContainer from '../tag/tag_index_container';
 
-
 class PhotoShow extends React.Component {
   constructor(props) {
     // console.log(props, 'photoshow');
@@ -66,11 +65,12 @@ class PhotoShow extends React.Component {
     // console.log(nextProps, 'nextprops');
     if (nextProps.match.params.photoId !== this.props.match.params.photoId) {
       nextProps.fetchPhoto(nextProps.match.params.photoId).then(() => {
-        this.setState({showModal: !!this.props.location.search});
+        this.setState({ showModal: !!this.props.location.search });
       });
     }
     this.setState({
-      photo: nextProps.photo});
+      photo: nextProps.photo
+    });
   }
 
   handleOpenModal() {
@@ -83,23 +83,25 @@ class PhotoShow extends React.Component {
 
   handleCommentSubmit(e) {
     e.preventDefault();
-    this.props.createComment(this.state.comment)
-      .then( () => window.scrollTo(0, document.body.scrollHeight));
-    this.setState({comment:
-      {body:'',
-      photo_id: this.props.match.params.photoId,
-      author_id: this.props.currentUser.id,
+    this.props
+      .createComment(this.state.comment)
+      .then(() => window.scrollTo(0, document.body.scrollHeight));
+    this.setState({
+      comment: {
+        body: '',
+        photo_id: this.props.match.params.photoId,
+        author_id: this.props.currentUser.id
       }
     });
   }
 
   handleTagSubmit(e) {
     e.preventDefault();
-    this.props.createTagging(this.state.tag.photo_id,this.state.tag.body);
+    this.props.createTagging(this.state.tag.photo_id, this.state.tag.body);
     this.setState({
       tag: {
         body: '',
-        photo_id: this.props.match.params.photoId,
+        photo_id: this.props.match.params.photoId
       }
     });
   }
@@ -117,13 +119,65 @@ class PhotoShow extends React.Component {
     if (this.props.photo.ownerOwns) {
       ownerButtons = (
         <div className="owner-btns">
-          <button className="photo-edit-btn" onClick={this.handleOpenModal}>Edit</button>
-          <button className="photo-del-btn" onClick={()=>this.props.deletePhoto(photo.id).then(() => this.props.history.goBack())}>
+          <button className="photo-edit-btn" onClick={this.handleOpenModal}>
+            Edit
+          </button>
+          <button
+            className="photo-del-btn"
+            onClick={() =>
+              this.props
+                .deletePhoto(photo.id)
+                .then(() => this.props.history.goBack())
+            }
+          >
             Delete
           </button>
         </div>
       );
     }
+
+    const photoEditModal = (
+      <ReactModal
+        isOpen={this.state.showModal}
+        contentLabel="onRequestClose"
+        onRequestClose={this.handleCloseModal}
+        className="photo-show-modal"
+        ariaHideApp={false}
+        overlayClassName="photo-show-overlay"
+      >
+        <form className="photo-edit-modal-form">
+          <label>
+            Title:
+            <input
+              type="text"
+              value={this.state.photo.title}
+              onChange={this.update('title')}
+              className="photo-edit-title"
+            />
+          </label>
+          <label>
+            Description:
+            <textarea
+              type="text"
+              value={this.state.photo.description}
+              onChange={this.update('description')}
+              className="photo-edit-description"
+            />
+          </label>
+          <button
+            type="button"
+            className="photo-modal-edit-btn"
+            onClick={() =>
+              this.props
+                .updatePhoto(this.state.photo)
+                .then(this.handleCloseModal())
+            }
+          >
+            Edit
+          </button>
+        </form>
+      </ReactModal>
+    );
 
     // tag button saved
     // <button type="button" className="tag-submit-btn"
@@ -133,45 +187,12 @@ class PhotoShow extends React.Component {
     return (
       <div className="show">
         <div className="show-image">
-          <img src={`${photo.img_url}`}/>
+          <img src={`${photo.img_url}`} />
         </div>
         <div className="show-bot-half">
-          <div className="show-image-info">
-            <div className="photo-show-top-row">
-              <h1 className="photo-show-title">{photo.title}</h1>
-              <div className="date-and-btns">
-                {ownerButtons}
-              </div>
-            </div>
-          </div>
+          <h1 className="photo-show-title">{photo.title}</h1>
           <h3 className="photo-show-description">{photo.description}</h3>
-          <ReactModal
-            isOpen={this.state.showModal}
-            contentLabel="onRequestClose"
-            onRequestClose={this.handleCloseModal}
-            className="photo-show-modal"
-            ariaHideApp={false}
-            overlayClassName="photo-show-overlay">
-            <form className="photo-edit-modal-form">
-              <label>Title:
-                <input
-                  type="text"
-                  value={this.state.photo.title}
-                  onChange={this.update('title')}
-                  className="photo-edit-title"/>
-              </label>
-              <label>Description:
-                <textarea
-                  type="text"
-                  value={this.state.photo.description}
-                  onChange={this.update('description')}
-                  className="photo-edit-description"/>
-              </label>
-              <button type="button" className="photo-modal-edit-btn"
-                onClick={()=>this.props.updatePhoto(this.state.photo)
-                  .then(this.handleCloseModal())}>Edit</button>
-            </form>
-          </ReactModal>
+          {photoEditModal}
           <div className="tag-and-comment-wrapper">
             <div className="comments">
               <form onSubmit={this.handleCommentSubmit}>
@@ -179,25 +200,32 @@ class PhotoShow extends React.Component {
                   placeholder="Add a comment"
                   onChange={this.updateComment('body')}
                   value={this.state.comment.body}
-                  className="comment-textarea"/>
+                  className="comment-textarea"
+                />
                 <button className="comment-btn">Comment</button>
               </form>
-              <CommentIndexContainer photo={photo} deleteComment={this.props.deleteComment}/>
+              <CommentIndexContainer
+                photo={photo}
+                deleteComment={this.props.deleteComment}
+              />
             </div>
 
             <div className="parent-tag-wrapper">
-              <h2 className="photo-date">Posted on {new Date(photo.created_at).toDateString()}</h2>
+              <h2 className="photo-date">
+                Posted on {new Date(photo.created_at).toDateString()}
+              </h2>
+              <div className="date-and-btns">{ownerButtons}</div>
               <h3 className="tag-label">Tags</h3>
               <form onSubmit={this.handleTagSubmit}>
                 <input
                   value={this.state.tag.body}
                   onChange={this.updateTag('body')}
                   type="text"
-                  placeholder="Add a tag"/>
+                  placeholder="Add a tag"
+                />
               </form>
-              <TagIndexContainer photo={photo}/>
+              <TagIndexContainer photo={photo} />
             </div>
-
           </div>
         </div>
       </div>
