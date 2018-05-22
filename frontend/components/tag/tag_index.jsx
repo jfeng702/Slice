6ef7677ef29b221818b39651c8228e5fdb4d13df
Joey Feng <jfeng702@gmail.com>
2018-05-22 16:13:01 -0700

    css caption overlay update

--- frontend/components/tag/tag_index.jsx
+++ frontend/components/tag/tag_index.jsx
@@ -2,24 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class TagIndex extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.buttonHandler = this.buttonHandler.bind(this);
+  }
   componentDidMount() {
     this.props.fetchTaggings(this.props.photo.id);
   }
 
+  buttonHandler(e) {
+    const width = e.target.getBoundingClientRect().width;
+    console.log(width);
+    console.log(e.target);
+    e.target.style.width = `${width * 2}px`;
+    console.log(e.target.style.width);
+  }
 
   render() {
     console.warn(this.props, 'props');
     // Tags link to a photo index of photos with that tag.
     return (
       <div className="tag-index-wrapper">
-        {
-          this.props.tags.map((tag,id) => (
+        {this.props.tags.map((tag, id) => (
+          <div>
             <Link className="tag-index-link" to={`/tags/${tag.body}/photos`}>
-              {tag.body}
+              <button
+                className="tag-btn"
+                onMouseEnter={this.buttonHandler}
+              >
+                {tag.body}
+              </button>
             </Link>
-          ))
-        }
+          </div>
+        ))}
       </div>
     );
   }
